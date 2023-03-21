168d1ea11769b2a2878df32c6448d29f191c045b
Moshiko Lev <moshiko@jit.io>
2023-03-21 16:21:59 +0200

    Update most-secret-file-ever.py

--- most-secret-file-ever.py
+++ most-secret-file-ever.py
@@ -6,5 +6,5 @@ PASSWORD="11234DFAS32!@!@"
 a = """HI!!!
 anotherwone !!!
 In The End it dosen4t even matter
-I have trieddddd so hard ddbut thats so far11
+I have trieddddd so hard ddbut thats so far11asdasd
 but in the End its dosent even matttterrr5rrrrr more code ... !!!!!"""
