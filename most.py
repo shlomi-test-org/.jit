62763d08ed3e312ec2f6fd1ce4b80c18ebe65802
Moshiko Lev <moshiko@jit.io>
2023-03-26 13:37:46 +0300

    Update most.py

--- most.py
+++ most.py
@@ -1,4 +1,4 @@
-secret="thisisthemostsecrettextevercreated"
+ssecret="thisisthemostsecrettextevercreated"
 AWS_KEY_SECRET = "ASD3FSA32121T523"
 SECRET="1241SFD232"
 PASSWORD="11234DFAS32!@!@"
@@ -7,6 +7,6 @@ a = """HI!!!
 anotherwone !!!asdsadassssd
 In The End it dosasdasden4t evenqwe asdasmasstter645645
 I have trieddddd sasdao hard ddbut thats so asdasd
-asdasd
+asdasdweq
 asdsadbut in the Endaswwd itsasdsad dosent eveASDasdAASDn asda more code ... !!!!!"""
 
