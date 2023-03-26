1d15e9030c1a7f63afd74f1088a7e3a63b7f5d63
Moshiko Lev <moshiko@jit.io>
2023-03-26 13:21:50 +0300

    Update most.py

--- most.py
+++ most.py
@@ -4,7 +4,7 @@ SECRET="1241SFD232"
 PASSWORD="11234DFAS32!@!@"
 
 a = """HI!!!
-anotherwone !!!asdsadasd
+anotherwone !!!asdsadassssd
 In The End it dosasdasden4t evenqwe asdasmasstter645645
 I have trieddddd sasdao hard ddbut thats so asdasd
 asdsadbut in the Endaswwd itsasdsad dosent eveASDasdAASDn asda more code ... !!!!!"""
