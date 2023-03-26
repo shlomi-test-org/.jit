27a3339a5184e4af7b96615e3ba6edc07f57c094
Moshiko Lev <moshiko@jit.io>
2023-03-26 13:43:01 +0300

    Update most.py

--- most.py
+++ most.py
@@ -6,7 +6,7 @@ PASSWORD="11234DFAS32!@!@"
 a = """HI!!!
 anotherwone !!!asdsadassssd
 In The End it dosasdasden4t evenqwe asdasmasstter645645
-I have trieddddd sasdao hard ddbasdaut thats so asdasd
+I havessadas trieddddd sasdao hard ddbasdaut thats so asdasd
 asdasdweq
 asdsadbut in the Endaswwd itsasdsad dosent eveASasdasDasdAASDn asda more code ... !!!!!"""
 
